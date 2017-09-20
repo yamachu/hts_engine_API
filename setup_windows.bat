8a4dd5ec20376accfb10981a5a7d2c7a84f4bd9b
Yusuke Yamada <yamachu.dev@gmail.com>
2017-09-20 19:32:40 +0900

    アーキテクチャの判定

--- setup_windows.bat
+++ setup_windows.bat
@@ -3,6 +3,10 @@ cd /d %~dp0
 git clone https://github.com/yamachu/World.git
 git clone https://github.com/yamachu/SPTK.git
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+if %ARCH% == x86 (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+) else (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+)
 
 nmake /f Makefile.mak hts_lib

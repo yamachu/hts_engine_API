abd021ef8aad1eaf2956852c9ee43f980b86b782
yamachu <yamachu.dev@gmail.com>
2021-08-19 20:09:28 +0900

    自分で環境変数をいい感じにして

--- setup_windows.bat
+++ setup_windows.bat
@@ -1,9 +1,3 @@
 cd /d %~dp0
 
-if %ARCH% == x86 (
-    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-) else (
-    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
-)
-
 nmake /f Makefile.mak hts_lib
